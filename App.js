5096e24b9c434e7e0cc21645196df848b9058673
praise456 <35904244+praise456@users.noreply.github.com>
2025-02-24 13:58:04 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FirstTimerForm from "./components/FirstTimerForm";
+
+function App() {
+  return (
+    <div className="App">
+      <FirstTimerForm />
+    </div>
+  );
+}
+
+export default App;
